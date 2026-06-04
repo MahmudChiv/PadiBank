3485c9b98652c4c7cb8e40a1f5d0196f5b6a560d
Adegboyega Mahmud Mobolaji <129618709+MahmudChiv@users.noreply.github.com>
2026-06-04 07:56:36 +0100

    fix(cors):Remove trailing slash from CORS origin URL

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
   app.enableCors({
-    origin: 'https://padibank-frontend.vercel.app/',
+    origin: 'https://padibank-frontend.vercel.app',
     credentials: true,
   });
   await app.listen(process.env.PORT ?? 3000);
